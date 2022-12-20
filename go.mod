20ffca25cabd5e87a88239ae901407065a363083
CS-5 <me@carsonseese.com>
2022-12-20 13:51:26 -0500

    Working initialization code

--- go.mod
+++ go.mod
@@ -5,7 +5,18 @@ go 1.19
 require github.com/urfave/cli/v2 v2.23.7
 
 require (
+	github.com/andreykaipov/goobs v0.12.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/kavu/go_reuseport v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/tidwall/evio v1.0.8 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
+	"time"
 
+	"github.com/andreykaipov/goobs"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
 var (
-	address string
-	port    int64
-	source  string
-	debug   bool
+	// OBS Websocket connection settings
+	address  string
+	port     int64
+	password string
+	source   string
 
-	listenAddress = "localhost"
+	// vMix Listener bind address and port
+	listenAddress = "0.0.0.0"
 	listenPort    = 8099
+
+	debug bool
+
+	oLog zerolog.Logger
+	vLog zerolog.Logger
 )
 
 func init() {
+	// Init CLI handler
 	app := &cli.App{
 		Name:        "SimpleTally",
 		Description: "A very simple tally for OBS Studio",
@@ -28,7 +39,7 @@ func init() {
 			&cli.StringFlag{
 				Name:     "address",
 				Value:    "127.0.0.1",
-				Usage:    "OBS Studio IP address",
+				Usage:    "obs-websocket address",
 				Aliases:  []string{"a"},
 				Required: false,
 			},
@@ -38,6 +49,12 @@ func init() {
 				Usage:    "obs-websocket port",
 				Aliases:  []string{"p"},
 				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "password",
+				Usage:    "obs-websocket password",
+				Aliases:  []string{"pw"},
+				Required: true,
 			},
 			&cli.StringFlag{
 				Name:     "source",
@@ -51,40 +68,81 @@ func init() {
 				Value:   false,
 			},
 		},
+
+		// Set globals to values from flags
 		Action: func(ctx *cli.Context) error {
 			address = ctx.String("address")
 			port = ctx.Int64("port")
 			source = ctx.String("source")
+			password = ctx.String("password")
+
 			debug = ctx.Bool("debug")
 
 			return nil
 		},
 	}
 
+	// Parse flags
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
+
+	// Init logger
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC822})
+
+	oLog = log.With().Str("component", "obs").Logger()
+	vLog = log.With().Str("component", "vMix").Logger()
 }
 
 func main() {
-	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", listenAddress, listenPort))
+	log.Info().Msg("Init finished, starting listeners...")
+
+	// Start OBS websocket connection
+	obs, err := goobs.New(fmt.Sprintf("%s:%d", address, port), goobs.WithPassword(password))
 	if err != nil {
-		fmt.Printf("Unable to start TCP listener on port: %d: %s\n", listenPort, err.Error())
+		oLog.Fatal().
+			AnErr("error", err).
+			Msgf("Unable to connect to OBS at '%s:%d' with password '%s'", address, port, password)
 	}
+	defer obs.Disconnect()
 
-	defer l.Close()
+	// Get info from OBS to verify the connection is working
+	info, err := obs.General.GetVersion()
+	if err != nil {
+		oLog.Error().
+			AnErr("error", err).
+			Msg("Unable to get OBS version information, this could mean there's a problem with the connection")
+	}
+	oLog.Info().Msgf("Connected to OBS (v%s) at '%s:%d'", info.ObsVersion, address, port)
 
-	log.Default().Printf("Listening on %s:%d", listenAddress, listenPort)
+	// Start TCP listener for vMix Tally clients
+	socket, err := net.Listen("tcp", fmt.Sprintf("%s:%d", listenAddress, listenPort))
+	if err != nil {
+		vLog.Fatal().
+			AnErr("error", err).
+			Msgf("Unable to start TCP listener on '%s:%d'", listenAddress, listenPort)
+	}
+	defer socket.Close()
+	vLog.Info().
+		Msgf("TCP listener started on '%s:%d'", listenAddress, listenPort)
+
+	// Handle each client connection in a separate thread
 	for {
-		conn, err := l.Accept()
+		conn, err := socket.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			vLog.Error().AnErr("error", err).Msg("Error accepting connection")
+			continue
 		}
 
-		go handle(conn)
+		go handleVMix(conn)
 	}
 }
 
-func handle(conn net.Conn) {
-
+func handleVMix(conn net.Conn) {
+	vLog.Info().Msgf("Connection from '%s' accepted", conn.RemoteAddr().String())
 }
